a56ac6d867dcb541452dcc65f816faee2d64dfa2
Eric <ericfoa@al.insper.edu.br>
2020-04-07 19:04:12 -0300

    Primeira tentativa

--- dna.c
+++ dna.c
@@ -16,13 +16,42 @@ void load_string(FILE *file, int *p, char *s, int t) {
 }
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
-  return 0;
+  int lcs = 0;
+
+  if (n == 0 || m == 0) {
+    length[n-1][m-1] = 0;
+    return 0;
+    }
+
+  if (a[n-2] == b[m-2]) {
+    if (length[n-2][m-2] == -1) {
+      length[n-2][m-2] = mlcs_w(a, n-1, b, m-1, length) + 1;
+      lcs = length[n-2][m-2];
+    }
+  }
+
+  else {
+    if (length[n-2][m-1] >= length[n-1][m-2]) {
+      return length[n-2][m-1];
+    }
+    else {
+      return length[n-1][m-2];
+    }
+  }
+  return lcs;
 }
 
 int mlcs(char a[], int n, char b[], int m) {
-  return 1;
+  int ni, mi, length[n][m];
+  for (ni = 0; ni < n; ni++){
+    for (mi = 0; mi < m; mi++){
+      length[ni][mi] = -1;
+    }
+  }
+  return mlcs_w(a, n, b, m, length);
 }
 
 int dlcs(char a[], int n, char b[], int m) {
-  return 2;
+
+  return 2
 }
